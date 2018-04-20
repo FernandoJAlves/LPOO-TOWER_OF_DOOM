a5ccf8e40d55dff654a14f2ccc10ec8b178f8d11
joao <joao-carlos.14@hotmail.com>
2018-04-20 15:10:09 +0100

    Camera implemented

--- core/src/game/logic/Game.java
+++ core/src/game/logic/Game.java
@@ -3,23 +3,30 @@ package game.logic;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Game extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	public static OrthographicCamera cam;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture(Gdx.files.internal("Stage2.png"));
+		cam = new OrthographicCamera(256,256);
+		cam.translate(128, 128);
+		
 	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        cam.update();
+		batch.setProjectionMatrix(cam.combined);
 		batch.begin();
 		batch.draw(img, 0, 0);
 		batch.end();
